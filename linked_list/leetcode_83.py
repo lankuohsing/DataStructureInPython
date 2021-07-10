1edad8e7693df9065bcf91360c89fde423cf0257
lankuohsing <lankuohsing@163.com>
2021-07-10 21:39:33 +0800

    add leetcode 83

---
+++ linked_list/leetcode_83.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 21:24:58 2021
+
+@author: lankuohsing
+"""
+
+ Definition for singly-linked list.
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        cur=head
+        while cur.next is not None:
+            if cur.val==cur.next.val:
+                cur.next=cur.next.next
+            else:
+                cur=cur.next
+        return head
