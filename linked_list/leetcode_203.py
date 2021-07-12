a6e16a32290ff58b13eb31eb8c6421df2195cd44
lankuohsing <lankuohsing@163.com>
2021-07-12 22:09:04 +0800

    add leetcode 203

---
+++ linked_list/leetcode_203.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 22:08:14 2021
+
+@author: lankuohsing
+"""
+
+ Definition for singly-linked list.
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head is None:
+            return head
+        while head is not None and head.val==val:
+            head=head.next
+
+        if head is None:
+            return head
+        cur=head
+        while cur.next is not None:
+            if cur.next.val==val:
+                cur.next=cur.next.next
+            else:
+                cur=cur.next
+        return head
