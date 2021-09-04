8e1b40e169183a1a5c4fb8a2cd9912cbcdac23f0
lankuohsing <lankuohsing@163.com>
2021-09-04 20:05:18 +0800

    complete 78 bin

--- search/backtrack/leetcode78-bin.py
+++ search/backtrack/leetcode78-bin.py
@@ -5,9 +5,13 @@ Created on Sun Aug 22 23:45:09 2021
 @author: lankuohsing
 """
 
-n=3
-for i in range(0,1<<n):# 2^n个子集，并且用二进制表示
-    for j in range(0,n):# 遍历每个元素
-        if i&(1<<j):# 该元素是否在这个子集里面出现
-            print(j,end="")
-    print("\n")
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        for i in range(0,1<<len(nums)):# 2^n个子集，并且用二进制表示
+            temp=[]
+            for j in range(0,len(nums)):# 遍历每个元素
+                if i&(1<<j):# 该元素是否在这个子集里面出现
+                    temp.append(nums[j])
+            result.append(temp)
+        return result
